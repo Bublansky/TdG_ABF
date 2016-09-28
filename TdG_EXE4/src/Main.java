52c3dc05cf45fef6e31812b60a8d338d0a8d18f4
Jhone Mendonça <g.m.enohj@gmail.com>
2016-09-28 17:23:55 -0400

    GG

--- TdG_EXE4/src/Main.java
+++ TdG_EXE4/src/Main.java
@@ -5,49 +5,42 @@ class Main
     
     public static void main(String[] args)
     {
-        int ordem, i, j;
+        int ordem, i, j, origem;
         Grafo g;
-        // leitura da ordem da matriz
+        // leitura da ordem-1 da matriz
         ordem = entrada.nextInt();
-        g = new Grafo(ordem);
+        g = new Grafo(ordem-1);
         // leitura da matriz de adjacencias
-        for(i = 1 ; i <= ordem ; i++)
+        for(i = 1 ; i < ordem ; i++)
         {
-            for(j = 1 ; j <= ordem ; j++)
+            for(j = 1 ; j < ordem ; j++)
             {
                 g.AddAresta(i, j, entrada.nextInt());
             }
         }
-        System.out.println(g.GetComponentesFortementeConectados());
+        origem = entrada.nextInt();
+        //g.ImprimirAdjacencias();
+        g.BellmanFord(origem+1);
+        
+        //System.out.println("ue");
+        //System.out.println(g.GetComponentesFortementeConectados());
         
         //g.ImprimirAdjacencias();
     }
     
     private static class Grafo
     {
-        private static final String BRANCO = "BRANCO";
-        private static final String CINZA = "CINZA";
-        private static final String PRETO = "PRETO";
-        
         private int adj[][];
         private int vertices = 0;
-        private String cor[];
         private int anterior[];
-        private int chegada[];
-        private int saida[];
-        private int tempo = 0;
-        private int verticesSaidaCrescente[];
-        private int cfc = 0;
+        private int distancia[];
 
         public Grafo(int quantidade)
         {
             vertices = quantidade;
             adj = new int [quantidade + 1][quantidade + 1];
-            cor = new String[quantidade + 1];
             anterior = new int[quantidade + 1];
-            chegada = new int[quantidade + 1];
-            saida = new int[quantidade + 1];
-            verticesSaidaCrescente = new int[quantidade + 1];
+            distancia = new int[quantidade + 1];
         }
         public int getAresta(int origem, int destino)
         {
@@ -57,114 +50,64 @@ class Main
         {
             adj[linha][coluna] = value;
         }
-        private void ordenarVerticesComMaiorTempoDeSaida()
+        public void BellmanFord(int origem)
         {
-            int copiaSaida[] = saida.clone();
-            int aux;
-            
-            int minimun;
-            for(int i = 1 ; i <= vertices ; i++)
+            int v, i, u, peso;
+            //passo 1:
+            for(v = 1 ; v <= vertices ; v++)
             {
-                minimun = i;
-                for(int j = 1; j <= vertices ; j++)
+                distancia[v] = Integer.MAX_VALUE;
+                anterior[v] = -1;
+            }
+            distancia[origem] = 0;
+            //passo 2:
+            for(i = 1 ; i < vertices ; i++)
+            {
+                for(u = 1 ; u <= vertices ; u++)
                 {
-                    if(copiaSaida[j] < copiaSaida[minimun])
+                    for(v = 1 ; v <= vertices ; v++)
                     {
-                        minimun = j;
-                    }
-                }
-                verticesSaidaCrescente[i] = minimun;
-                copiaSaida[minimun] = Integer.MAX_VALUE;
-            }
-            
-            //verticesSaidaDecrescente = saida.clone();
-            //Mergesort_ND(verticesSaidaDecrescente, 1, vertices);
-           
-            //ImprimirVetor(verticesSaidaCrescente);
-            //Mergesort_ND(saida, 1, vertices);
-            //ImprimirVetor(saida);
-        }
-       
-        public void DFS()
-        {
-            for(int u = 1 ; u <= vertices ; u++)
-            {
-                cor[u] = BRANCO;
-                anterior[u] = -1;
-            }
-            tempo = 0;
-            for(int u = 1 ; u <= vertices ; u++)
-            {
-                if(cor[u].equals(BRANCO))
-                {
-                    DFS_Visita(u);
-                }
-            }
-        }
-        public void DFS_Visita(int u)
-        {
-            tempo++;
-            chegada[u] = tempo;
-            cor[u] = CINZA;
-            for(int v = 1 ; v <= vertices ; v++)    //para cada vertice adjacente a u
-            {
-                if(getAresta(u, v) == 1)    //se tiver aresta de u para i
-                {
-                    if(cor[v].equals(BRANCO))
-                    {
-                        anterior[u] = v;
-                        DFS_Visita(v);  //visita v
+                        peso = getAresta(u, v);
+                        if(peso != 0)
+                        {    
+                            if(distancia[u] + peso < distancia[v])
+                            {
+                                distancia[v] = distancia[u] + peso;
+                                anterior[v] = u;
+                            }
+                        }
                     }
                 }
             }
-            cor[u] = PRETO;     //marca como visitado
-            tempo++;
-            saida[u] = tempo;   //marca o tempo de saida
+            //passo 3:
+            for(u = 1 ; u <= vertices ; u++)
+            {
+                for(v = 1 ; v <= vertices ; v++)
+                {
+                    peso = getAresta(u, v);
+                    if(peso != 0)
+                    {
+                        if(distancia[u] + peso < distancia[v])
+                        {
+                            System.out.println("ciclo negativo");
+                            System.exit(0);
+                        }
+                    }
+                }
+            }
+            for(i = 1 ; i <= vertices ; i++)
+            {
+                if(distancia[i] == Integer.MAX_VALUE)
+                {
+                    System.out.print("i ");
+                }
+                else
+                {
+                    System.out.print(distancia[i] + " ");
+                }
+            }
         }
         
-        public void DFS_Gt()
-        {
-            int u;
-            for(u = vertices ; u > 0 ; u--)
-            {
-                cor[u] = BRANCO;
-                //anterior[verticesSaidaCrescente[u]] = -1;
-            }
-            //tempo = 0;
-            for(u = vertices ; u > 0 ; u--)
-            {
-                //System.out.println("testando o "+verticesSaidaCrescente[u]);
-                if(cor[verticesSaidaCrescente[u]].equals(BRANCO))
-                {
-                    //System.out.println("o vertice " + verticesSaidaCrescente[u]+" eh branco");
-                    cfc++;
-                    DFS_Visita_Gt(verticesSaidaCrescente[u]);
-                }
-            }
-        }
-        public void DFS_Visita_Gt(int u)
-        {
-            //tempo++;
-            //chegada[u] = tempo;
-            cor[u] = CINZA;
-            //System.out.println("visitando " + u);
-
-            for(int v = 1 ; v <= vertices ; v++)    //para cada vertice adjacente a u
-            {
-                if(getAresta(v, u) == 1)    //se tiver aresta de v para u
-                {
-                    //System.out.println("vizinho transposto de " +u+ ": " + v);
-                    if(cor[v].equals(BRANCO))
-                    {
-                        //anterior[u] = v;
-                        DFS_Visita_Gt(v);  //visita v
-                    }
-                }
-            }
-            cor[u] = PRETO;     //marca como visitado
-            //tempo++;
-            //saida[u] = tempo;   //marca o tempo de saida
-        }
         public void ImprimirVetor(int A[])
         {
             System.out.print("\n<!------------------------------------------------------->\n");
@@ -185,15 +128,6 @@ class Main
                 }
                 System.out.println("");
             }
-        }
-        public int GetComponentesFortementeConectados()
-        {
-            DFS();
-            ordenarVerticesComMaiorTempoDeSaida();
-            DFS_Gt();
-            //ImprimirVetor(verticesSaidaCrescente);
-            //ImprimirVetor(saida);
-            return cfc;
         }
     }
 }
