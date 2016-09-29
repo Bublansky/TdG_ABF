3f68a104159b073bcdf7681075234b04185e6621
Jhone Mendonça <g.m.enohj@gmail.com>
2016-09-29 15:33:21 -0400

    6/10

--- TdG_EXE4/src/Main.java
+++ TdG_EXE4/src/Main.java
@@ -9,18 +9,19 @@ class Main
         Grafo g;
         // leitura da ordem-1 da matriz
         ordem = entrada.nextInt();
-        g = new Grafo(ordem-1);
+        ordem--;
+        g = new Grafo(ordem);
         // leitura da matriz de adjacencias
-        for(i = 1 ; i < ordem ; i++)
+        for(i = 0 ; i < ordem ; i++)
         {
-            for(j = 1 ; j < ordem ; j++)
+            for(j = 0 ; j < ordem ; j++)
             {
                 g.AddAresta(i, j, entrada.nextInt());
             }
         }
         origem = entrada.nextInt();
         //g.ImprimirAdjacencias();
-        g.BellmanFord(origem+1);
+        g.BellmanFord(origem);
         
         //System.out.println("ue");
         //System.out.println(g.GetComponentesFortementeConectados());
@@ -32,15 +33,13 @@ class Main
     {
         private int adj[][];
         private int vertices = 0;
-        private int anterior[];
         private int distancia[];
 
         public Grafo(int quantidade)
         {
             vertices = quantidade;
-            adj = new int [quantidade + 1][quantidade + 1];
-            anterior = new int[quantidade + 1];
-            distancia = new int[quantidade + 1];
+            adj = new int [quantidade][quantidade];
+            distancia = new int[quantidade];
         }
         public int getAresta(int origem, int destino)
         {
@@ -54,48 +53,47 @@ class Main
         {
             int v, i, u, peso;
             //passo 1:
-            for(v = 1 ; v <= vertices ; v++)
+            for(v = 0 ; v < vertices ; v++)
             {
                 distancia[v] = Integer.MAX_VALUE;
-                anterior[v] = -1;
             }
             distancia[origem] = 0;
             //passo 2:
-            for(i = 1 ; i < vertices ; i++)
+            for(i = 0 ; i < vertices - 1; i++)
             {
-                for(u = 1 ; u <= vertices ; u++)
+                for(u = 0 ; u < vertices ; u++)
                 {
-                    for(v = 1 ; v <= vertices ; v++)
+                    for(v = 0 ; v < vertices ; v++)
                     {
                         peso = getAresta(u, v);
                         if(peso != 0)
                         {    
-                            if(distancia[u] + peso < distancia[v])
+                            if(distancia[u] + peso < distancia[v] && distancia[u] != Integer.MAX_VALUE)
                             {
                                 distancia[v] = distancia[u] + peso;
-                                anterior[v] = u;
                             }
                         }
                     }
                 }
             }
             //passo 3:
-            for(u = 1 ; u <= vertices ; u++)
+            for(u = 0 ; u < vertices ; u++)
             {
-                for(v = 1 ; v <= vertices ; v++)
+                for(v = 0 ; v < vertices ; v++)
                 {
                     peso = getAresta(u, v);
+                    //se ha uma aresta uv
                     if(peso != 0)
                     {
-                        if(distancia[u] + peso < distancia[v])
+                        if(distancia[u] + peso < distancia[v] && distancia[u] != Integer.MAX_VALUE)
                         {
                             System.out.println("ciclo negativo");
-                            System.exit(0);
+                            return;
                         }
                     }
                 }
             }
-            for(i = 1 ; i <= vertices ; i++)
+            for(i = 0 ; i < vertices ; i++)
             {
                 if(distancia[i] == Integer.MAX_VALUE)
                 {
@@ -105,28 +103,6 @@ class Main
                 {
                     System.out.print(distancia[i] + " ");
                 }
-            }
-        }
-        
-        public void ImprimirVetor(int A[])
-        {
-            System.out.print("\n<!------------------------------------------------------->\n");
-            for(int i = 1 ; i < A.length ; i++)
-            {
-                System.out.print(A[i] + "  ");
-            }
-        }
-        public void ImprimirAdjacencias()
-        {
-            System.out.print("\n<!------------------------------------------------------->\n");
-            int i, j;
-            for(i = 1 ; i <= vertices ; i++)
-            {
-                for(j = 1 ; j <= vertices ; j++)
-                {
-                    System.out.print(adj[i][j] + " ");
-                }
-                System.out.println("");
             }
         }
     }
